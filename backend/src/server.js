383f2b108a7fbfdb77b9606caa270b277145f3e4
Anthony <anthony.silva@aluno.ifsp.edu.br>
2024-11-11 10:55:58 -0300

    Não apaguem pls eu suplico

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,14 @@
-console.log("Olá, mundo!")
+const {WebSocketServer} = require("ws")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080})
+
+wss.on("connection", (ws) => {
+    ws.on("error", console.error)
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+    console.log("Client connected")
+})
